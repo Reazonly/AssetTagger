0ac98a17df6c85b4d5303d35e8e31fb0af1739f5
rafli727 <rafliggwp321@gmail.com>
2025-07-31 09:27:37 +0800

    tes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,19 +61,21 @@
 
             {{-- Navigation Menu --}}
             <nav class="mt-6 flex-grow px-4">
-                <a href="{{ route('dashboard') }}" class="flex items-center px-4 py-3 rounded-lg transition-colors duration-200 {{ request()->routeIs('dashboard') ? 'bg-jg-green' : 'hover:bg-white/20' }}">
-                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
-                    </svg>
-                    <span class="mx-4 font-medium">Dashboard</span>
-                </a>
-         <a href="{{ route('assets.index') }}" class="mt-2 flex items-center px-4 py-3 rounded-lg transition-colors duration-200 {{ request()->routeIs('assets.*') ? 'bg-jg-green' : 'hover:bg-white/20' }}">
-         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/>
+    <a href="{{ route('dashboard') }}" class="flex items-center px-4 py-3 rounded-lg transition-colors duration-200 
+        {{ request()->routeIs('dashboard') ? 'bg-jg-green text-white' : 'text-gray-600 hover:bg-white/20 hover:text-gray-800' }}">
+        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
         </svg>
-         <span class="mx-4 font-medium">Manajemen Aset </span>
-         </a>
-         </nav>
+        <span class="mx-4 font-medium">Dashboard</span>
+    </a>
+    <a href="{{ route('assets.index') }}" class="mt-2 flex items-center px-4 py-3 rounded-lg transition-colors duration-200 
+        {{ request()->routeIs('assets.*') ? 'bg-jg-green text-white' : 'text-gray-600 hover:bg-white/20 hover:text-gray-800' }}">
+        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/>
+        </svg>
+        <span class="mx-4 font-medium">Manajemen Aset </span>
+    </a>
+</nav>
 
 
             {{-- User Info & Logout --}}
