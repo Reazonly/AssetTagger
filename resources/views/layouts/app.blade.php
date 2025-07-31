b2c21a8ea65e51495fec06fab5a485dc6487a8cd
rafli727 <rafliggwp321@gmail.com>
2025-07-31 08:35:16 +0800

    tes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,10 +67,14 @@
                     </svg>
                     <span class="mx-4 font-medium">Dashboard</span>
                 </a>
-                <a href="{{ route('assets.index') }}" class="mt-2 flex items-center px-4 py-3 rounded-lg transition-colors duration-200 {{ request()->routeIs('assets.*') ? 'bg-jg-green' : 'hover:bg-white/20' }}">
-                    <span class="mx-4 font-medium">Manajemen Aset</span>
-                </a>
-            </nav>
+         <a href="{{ route('assets.index') }}" class="mt-2 flex items-center px-4 py-3 rounded-lg transition-colors duration-200 {{ request()->routeIs('assets.*') ? 'bg-jg-green' : 'hover:bg-white/20' }}">
+         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/>
+        </svg>
+         <span class="mx-4 font-medium">Manajemen Aset Jhonlin Group</span>
+         </a>
+         </nav>
+
 
             {{-- User Info & Logout --}}
             @auth
