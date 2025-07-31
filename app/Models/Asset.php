500e096bd0bbadd915191e5d4d7b8c9faaef04d9
rafli727 <rafliggwp321@gmail.com>
2025-07-31 11:18:50 +0800

    tes

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // Import untuk SoftDeletes
 
 class Asset extends Model
 {
-    use HasFactory;
-    protected $guarded = ['id'];
+    // Menggunakan HasFactory dan SoftDeletes
     use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be cast.
@@ -29,6 +31,7 @@ class Asset extends Model
 
     public function history()
     {
+        // Menambahkan orderBy untuk mengurutkan riwayat dari yang terbaru
         return $this->hasMany(AssetHistory::class)->orderBy('tanggal_mulai', 'desc');
     }
 }
