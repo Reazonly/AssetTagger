c466526239d81279579bbff6feed7fa1b67c0a0f
rafli727 <rafliggwp321@gmail.com>
2025-07-31 09:34:08 +0800

    tes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,16 +60,16 @@
             </div>
 
             {{-- Navigation Menu --}}
-            <nav class="mt-6 flex-grow px-4">
+         <nav class="mt-6 flex-grow px-4">
     <a href="{{ route('dashboard') }}" class="flex items-center px-4 py-3 rounded-lg transition-colors duration-200 
-        {{ request()->routeIs('dashboard') ? 'bg-jg-green text-white' : 'text-gray-600 hover:bg-white/20 hover:text-gray-800' }}">
+        {{ request()->routeIs('dashboard') ? 'bg-jg-green text-gray-200' : 'text-white hover:bg-white/20 hover:text-gray-200' }}">
         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
         </svg>
         <span class="mx-4 font-medium">Dashboard</span>
     </a>
     <a href="{{ route('assets.index') }}" class="mt-2 flex items-center px-4 py-3 rounded-lg transition-colors duration-200 
-        {{ request()->routeIs('assets.*') ? 'bg-jg-green text-white' : 'text-gray-600 hover:bg-white/20 hover:text-gray-800' }}">
+        {{ request()->routeIs('assets.*') ? 'bg-jg-green text-gray-200' : 'text-white hover:bg-white/20 hover:text-gray-200' }}">
         <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"/>
         </svg>
