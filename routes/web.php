e487cee6e8065d16d32a23cd0254ed1a30764eb2
Reazonly <noorandika58@gmail.com>
2025-08-15 15:24:55 +0800

    tebaru

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use App\Http\Controllers\RoleController;
 // Authentication Routes
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
-Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [RegisterController::class, 'register']);
+// Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+// Route::post('register', [RegisterController::class, 'register']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
 // Public Routes
